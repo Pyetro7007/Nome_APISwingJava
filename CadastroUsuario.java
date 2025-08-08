fbac24648968fcf4a7506093e0ee80606f68c6b5
Pyetro7007 <pyetrofabricio2014@gmail.com>
2025-08-08 19:36:37 -0300

    Completar os desafios

--- CadastroUsuario.java
+++ CadastroUsuario.java
@@ -5,36 +5,42 @@ import java.awt.event.*;
 public class CadastroUsuario {
     public static void main(String[] args) {
         // Criação do frame (janela)
-        JFrame frame = new JFrame("Exemplo Simples com Swing");
+        JFrame frame = new JFrame("Cadastro de Usuario");
         frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new FlowLayout());
 
         // Rótulos 
-        JLabel labelNome = new JLabel("Nome:");
-        JLabel labelSobrenome = new JLabel("Sobrenome:");
-
+        JLabel labelNomeCompleto = new JLabel("Nome Completo:");
+        JLabel labelEmail = new JLabel("Email:");
+        JLabel labelIdade = new JLabel("Idade:");
+        
         // Campos de Texto
-        JTextField campoNome = new JTextField(10);
-        JTextField campoSobrenome = new JTextField(10);
+        JTextField campoNomeCompleto = new JTextField(35);
+        JTextField campoEmail = new JTextField(25);
+        JTextField campoIdade = new JTextField(3);
 
         // Botão
-        JButton botaoSaudar = new JButton("Saudar");
+        JButton botaoCadastrar = new JButton("Cadastrar");
         
         // Ação do botão
-        botaoSaudar.addActionListener(new ActionListener() {
+        botaoCadastrar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                String nomeCompleto = campoNome.getText() + " " + campoSobrenome.getText();
-                JOptionPane.showMessageDialog(frame, "Olá, " + nomeCompleto + "!");
+                String nome = campoNomeCompleto.getText();
+                String email = campoEmail.getText();
+                String idade = campoIdade.getText();
+                JOptionPane.showMessageDialog(frame, "Usuário Cadastrado:" + "\nNome: " + nome + "\nEmail: " + email + "\nIdade: " + idade);
             }
         });
 
         // Adincionando componentes ao frame
-        frame.add(labelNome);
-        frame.add(campoNome);
-        frame.add(labelSobrenome);
-        frame.add(campoSobrenome);
-        frame.add(botaoSaudar);
+        frame.add(labelNomeCompleto);
+        frame.add(campoNomeCompleto);
+        frame.add(labelEmail);
+        frame.add(campoEmail);
+        frame.add(labelIdade);
+        frame.add(campoIdade);
+        frame.add(botaoCadastrar);
 
         // Exibir a janela
         frame.setVisible(true);
